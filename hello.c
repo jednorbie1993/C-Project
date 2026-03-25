b6a55e03d6e07fdcdab8370fd7b21ebbdf91c88a
jed jarin <jedjarin@gmail.com>
2026-03-25 22:15:37 +0900

    review

--- hello.c
+++ hello.c
@@ -4,3 +4,37 @@ int main() {
     printf("Hello GitHub!\n");
     return 0;
 } //
+
+/*
+Kung gusto mo i-level up pa (next step)
+
+Pwede natin gawin:
+
+Option 1:
+
+Login system + file (real app feel)
+
+Option 2:
+
+Add more fields (age, course, etc)
+
+Option 3:
+
+Convert to web (HTML + JS)
+
+Honest assessment sayo
+
+Kung kaya mo explain yung code na yan kahit:
+
+50–60%
+
+hindi ka na beginner
+ nasa “early intermediate” ka na
+
+Kung gusto mo next level challenge:
+
+ “gawin natin login system”
+ or “gawin natin mas real (may roles/admin)”
+
+Sasabayan kita step-by-step—hindi ka malulunod 
+*/
