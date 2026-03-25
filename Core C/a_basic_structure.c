c138d72f44786b9d5fa4b9cd64facf07ac5e1205
jed jarin <jedjarin@gmail.com>
2026-03-25 22:24:15 +0900

    practice

---
+++ Core C/a_basic_structure.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+//(Skeleton ng C Program)
---
+++ Core C/b_variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    float price = 99.5;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Price: %.2f\n", price);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
+
+//data types
